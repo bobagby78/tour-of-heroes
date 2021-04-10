4304b90676d276477253a3e93dc19615ba3b4e94
bobagby78 <bobagby78@gmail.com>
2021-04-10 13:10:31 -0500

    finished step 1 of the project

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,10 +6,15 @@ import { Hero } from '../hero';
   templateUrl: './heroes.component.html',    
   styleUrls: ['./heroes.component.css']    
 })
-export class HeroesComponent implements OnInit { 
-  hero = 'Windstorm'
-  constructor() {  
 
+export class HeroesComponent implements OnInit { 
+  // hero = 'Windstorm'  !!! in this old line of code, hero was the variable, the name was Windstorm. It was displayed on the view as a string
+  hero: Hero = { //the new code declares a variable and says it's of the Hero interface (kinda like a class)
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() {  
 
   }
 
