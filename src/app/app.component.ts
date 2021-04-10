b6f3cb83d1a03b11e4bf5559622acbcdafc2cc8f
bobagby78 <bobagby78@gmail.com>
2021-04-10 11:04:19 -0500

    added comments to keep track of what's happening

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,15 @@
+/*
+Everything copied on lines 3 - 11 is automatically generated with ng new tour-of-heroes
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = "My mother's house";
+}*/
 import { Component } from '@angular/core';
 
 @Component({
