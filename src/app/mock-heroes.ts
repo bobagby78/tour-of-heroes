1d3df8bdc021b1404683efad999f84633e967215
bagbyj <bagbyj@wesclin.org>
2021-04-10 17:56:39 -0500

    done with phase 2

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,7 +1,7 @@
 import { Hero } from './hero';
 
 export const mockHeroes: Hero[] = [     //creates a new variable HEROES, based on the Hero interface, but an array[] 
-    {id: 11, name: 'Jimmy the bitch'},   //each item in the array of Heroes is an object with an id and a name:  {id: , name: }
+    {id: 11, name: 'Jimmy the snitch'},   //each item in the array of Heroes is an object with an id and a name:  {id: , name: }
     {id: 12, name: 'Marge the fatty'},
     {id: 13, name: 'Robert the nerd'},
     {id: 14, name: 'Fred the creeper'},
