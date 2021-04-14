2295fe72202c56eb41009545721078b9e7aea430
bobagby78 <bobagby78@gmail.com>
2021-04-14 18:49:42 -0500

    finished the detail appearing functionality

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,25 +10,24 @@ import { mockHeroes } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit { 
   // hero = 'Windstorm'  !!! in this old line of code, hero was the variable, the name was Windstorm. It was displayed on the view as a string
-  hero: Hero = { //the new code declares a variable and says it's of the Hero interface (kinda like a class)
-    id: 1,
-    name: 'Windstorm'
+  // hero: Hero = { //the new code declares a variable and says it's of the Hero interface (kinda like a class)
+  //   id: 1,
+  //   name: 'Windstorm'
 
-  };
+  // };
+  
   heroes = mockHeroes
-
+  selectedHero?: Hero;
+  
   constructor() {  
 
-  }
-
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
   
   
   ngOnInit() {
 
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
